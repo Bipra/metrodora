3b9ef5dda716d01411fa640dc42b1474976b4a47
Bipra <55776768+Bipra@users.noreply.github.com>
2026-01-16 00:16:44 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,6 @@
+const medicines = [
+  {id:1, brand:"Crocin 500", salt:"Paracetamol", strength:"500 mg", price:25, manufacturer:"GSK"},
+  {id:2, brand:"Calpol 500", salt:"Paracetamol", strength:"500 mg", price:20, manufacturer:"GSK"},
+  {id:3, brand:"Pacimol 500", salt:"Paracetamol", strength:"500 mg", price:18, manufacturer:"Ipca"},
+  {id:4, brand:"Dolo 650", salt:"Paracetamol", strength:"650 mg", price:30, manufacturer:"Micro Labs"}
+];
